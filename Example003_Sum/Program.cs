93044c32d5280c8062a745720ace061253e8cdc7
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-06 01:18:44 +0300

    Update Example003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+
+Console.WriteLine($"A = {numberA}");
+Console.WriteLine($"B = {numberB}");
 
 Console.WriteLine(numberA + numberB);
