1f3793a5879d23c061e4ff667380dae0a9508c16
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-06 00:57:32 +0300

    Apend Example002 User Name

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string userName;
+
+Console.WriteLine("Enter User Name: ");
+userName = Console.ReadLine();
+
+Console.WriteLine($"Hello, {userName}");
